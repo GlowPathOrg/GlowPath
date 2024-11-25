ec318cd531ca4fd2225118a5661d6e293a08d41b
Melisaysnuh <melissacessna@yahoo.com>
2024-11-25 17:07:39 +0100

    dark mode

--- client/src/components/map-component.tsx
+++ client/src/components/map-component.tsx
@@ -1,24 +1,43 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import './map-component.css'
+import { useState } from 'react';
 
 // note this will eventually be a prop, should look like this!
 
 
 const MapComponent: React.FC = () => {
     const mockLocation = [52.4771, 13.4310];
-    const homeMapClass = { height: "80vh", width: "90vw" };
+    const [isDarkMode, setDarkMode] = useState(true);
+
+    const toggleDarkMode = () => {
+        console.log('dark mode is: ', isDarkMode);
+        setDarkMode(!isDarkMode)
+    }
+    console.log('dark mode is now: ', isDarkMode);
+
 
 
     return (
-        <MapContainer center={mockLocation} zoom={13} scrollWheelZoom={false}
-        style={homeMapClass}>
+        <MapContainer key={isDarkMode ? 'dark' : 'light'}
+            center={mockLocation}
+            zoom={13}
+            scrollWheelZoom={false}
+            className={isDarkMode ? 'dark' : 'light'}>
+            <div className='map-controls'>
+                <input
+                    placeholder='search...'></input>
+                <button>Go</button>
+                <button onClick={toggleDarkMode}>{isDarkMode ? 'Go Light' : 'Go Dark'}</button>
+
+            </div>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={mockLocation}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Here you are!
                 </Popup>
             </Marker>
         </MapContainer>
