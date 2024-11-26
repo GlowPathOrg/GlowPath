4ae0641d5fc6509ac2a8171e544fc772c4e13199
JonasCodeworks <codeworks@artyfakt.org>
2024-11-26 12:30:08 +0100

    fix: add manifest to vite config to enable PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,43 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        name: 'GlowPath',
+        short_name: 'GlowPath',
+        description: 'This is where the app description goes',
+        icons: [
+          {
+            src: 'icon-32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+        ]
       }
     })
   ],
