6be3995ba2d17ff74d3d00f6d4f205e0bb2f6fe5
Melisaysnuh <melissacessna@yahoo.com>
2024-11-27 12:05:49 +0100

    fixed two small bugs in build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import JourneyPage from './pages/journeyPage/JourneyPage';
 import WhereToPage from './pages/WhereToPage';
-import MapComponent from './components/MapComponent';
+
 
 
 /* import HistoryPage from
