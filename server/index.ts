f6890d8f071e07b5e26ad506bc89dc9172470a8c
JonasCodeworks <codeworks@artyfakt.org>
2024-11-27 16:57:52 +0100

    wip: routes for shares

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,41 @@ import { setupSocket } from "./socket" // This is needed to setup socket.io
 const app = express();
 const PORT = 3000;
 const server = setupSocket(app); // This is needed to setup socket.io
+import { v4 as uuid } from 'uuid';
 
 // ...
 
+interface Room {
+  user: string;
+  route: string;
+  password: string;
+}
+
+const rooms: {[key: string]: Room} = {};
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/journey", (req, res) => {
+  res.sendFile(__dirname + "/sender.html");
+})
+
+app.get("/share/create", (req, res) => {
+  const id = uuid();
+  const room = {user: "Testuser", route: "Testroute", password: "Testpassword"};
+  rooms[id] = room;
+  res.json({...rooms[id], id})
+});
+
+app.get("/observe", (req, res) => {
+  res.sendFile(__dirname + "/receiver.html");
+})
+
+app.get("/share/:id", (req, res) => {
+
 });
 
 server.listen(PORT, () => {
