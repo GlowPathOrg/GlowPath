27406b610864fd0c2e9e0b1f3dccb0fd7f24d885
JonasCodeworks <codeworks@artyfakt.org>
2024-11-28 14:10:52 +0100

    wip

--- server/routes/shareRoutes.ts
+++ server/routes/shareRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createShare, accessShare } from "../controllers/shareControllers";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const shareRoutes = express.Router();
 
-shareRoutes.post("/share", /* authMiddleware */ createShare); // TODO: uncomment authMiddleware when ready
-shareRoutes.get("/share/:id", accessShare);
+shareRoutes.post("/share", authMiddleware, createShare); // TODO: uncomment authMiddleware when ready
+shareRoutes.post("/share/:id", accessShare);
 
 export default shareRoutes;
