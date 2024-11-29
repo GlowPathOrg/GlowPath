17f8170c42bb73b07b2d3f6bc2252ec9354263bc
JonasCodeworks <codeworks@artyfakt.org>
2024-11-29 12:28:28 +0100

    feat: integrating new route model

--- server/Types/share.ts
+++ server/Types/share.ts
@@ -1,8 +1,9 @@
 import { UserI } from "./user";
+import { RouteI } from "./Route";
 
 export interface ShareI {
   _id?: string;
   owner: UserI;
-  route: string; // TODO: This should eventually correspond to the route type
+  route: RouteI;
   password: string;
 }
