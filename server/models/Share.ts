a8078bc109f5cd62444a1e9dc3aa51663e53f5b0
Melisaysnuh <melissacessna@yahoo.com>
2024-11-29 11:36:31 +0100

    undid adjustments to Share schema

--- server/models/Share.ts
+++ server/models/Share.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import { ShareI } from "../@Types/share";
-import { RouteSchema } from "./Route";
+import { ShareI } from "../Types/share";
 
 const shareSchema = new mongoose.Schema<ShareI>({
-  owner: { type: userSchema, required: true }, // TODO: Replace with reference to User schema when ready
-  route: { type: RouteSchema, required: true }, // TODO: Replace with reference to Route schema when ready
+  owner: { type: String, required: true }, // TODO: Replace with reference to User schema when ready
+  route: { type: String, required: true }, // TODO: Replace with reference to Route schema when ready
   password: { type: String, required: true }
 })
 
