9b2d46233d5a6f777ad111555bf5586a82860eed
Melisaysnuh <melissacessna@yahoo.com>
2024-12-05 00:49:21 +0100

    open cors

--- client/src/pages/journeyPage/JourneyPage.tsx
+++ client/src/pages/journeyPage/JourneyPage.tsx
@@ -174,7 +174,7 @@ useEffect(() => {
       const route: RouteI = {polyline: currentRoute, instructions: currentInstructions, summary: currentSummary};
       console.log('sharing' , route)
       const result = await createShare(route);
-      console.log(`UPDATED PAGE:  https://glowpathorg.github.io/GlowPath/observe/:${result.data.id}?password=${result.data.password}`)
+      console.log(`UPDATED PAGE:  https://glowpathorg.github.io/GlowPath/observe/${result.data.id}?password=${result.data.password}`)
       setShareId(result.data.id);
 
     } catch (err) {
